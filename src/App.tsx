1c631433803202061d96bab04a4ac4d3ac4aefc1
Jaydeep Patel <jaydeep.mobilefirst@gmail.com>
2024-03-06 11:00:52 +0530

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useSelector } from 'react-redux';
-import { RootState } from './redux/store';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import Landing from "./pages/Landing";
 
 function App() {
-  const {value} = useSelector((state : RootState) => state.common)
+  const { value } = useSelector((state: RootState) => state.common);
 
-  console.log({value})
+  console.log({ value });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Landing />
     </div>
   );
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "../components/landingPage/Heading";
+
+const Landing = () => {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+};
+
+export default Landing;
