ca5dd9b3f28c1801c7ffaf8c586c9093ceb7b703
pratibha1897 <pratibha18bharti@gmail.com>
2024-03-11 19:40:16 +0530

    varify btn

---
+++ src/components/userFlow/depositeTaker/VarifyDetailsBtn.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const VerifyDetailsBtn = (props: Props) => {
+  return (
+    <div>
+      <button className="rounded-[12px] bg-[#385723] text-[#ffffff] border pt-[12px] pr-[22px] pb-[12px] pl-[22px]">
+        Varify details
+      </button>
+    </div>
+  );
+};
+
+export default VerifyDetailsBtn;
