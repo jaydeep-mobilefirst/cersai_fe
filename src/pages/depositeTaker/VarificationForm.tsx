1fd4f8ade1113353b448d79493863752d752ffea
unknown <rohan.mobilefirst@gmail.com>
2024-05-04 19:51:31 +0530

    Pincode Pan verification and OTP modal integrated

--- src/pages/depositeTaker/VarificationForm.tsx
+++ src/pages/depositeTaker/VarificationForm.tsx
@@ -7,12 +7,13 @@ import SelectButton from "../../components/userFlow/form/SelectButton";
 import { useContext, useState } from "react";
 import { FormHandlerContext } from "../../contextAPI/useFormFieldHandlers";
 import LoaderSpin from "../../components/LoaderSpin";
+import axios from "axios";
 
 type Props = {};
 
 const VerificationForm = (props: Props) => {
   const [loader, setLoader] = useState(false);
-  const {onChange, handleValidationChecks} = useContext(FormHandlerContext)
+  const {onChange, handleValidationChecks, updatePanFormField} = useContext(FormHandlerContext)
   const Navigate = useNavigate();
   const {allFormData, setAllFormData} = useDepositTakerRegistrationStore(state => state)
   const sectionId = allFormData?.entitySections?.find((s : any) => s?.sectionName === "Verification");
@@ -22,11 +23,40 @@ const VerificationForm = (props: Props) => {
  
   const onSubmit = async (event : any) => {
     event?.preventDefault();
+
+    // Verify Pan
+
+    const verifyPan = async () : Promise<boolean> => {
+      try {
+        let company = formFields?.find((field : any, i : number) => field?.label === "Company Name");
+        let pan = formFields?.find((field : any, i : number) =>  field?.label === "Pan Number");
+        
+        let response = await axios.post("http://34.149.91.231/cms/pandirectory/api", {
+          name:company?.userInput?.toUpperCase(),
+          pan_no: pan?.userInput
+        })
+        const data = response.data;
+        
+        const panUpdate = updatePanFormField(data, pan);
+
+        return panUpdate;
+        
+      } catch (error) {
+        alert("Error while verifying pan, Please try later!")
+        return false
+      }
+
+    }
+
     setLoader(true)
-    const noError = await handleValidationChecks(formFields)    
+    const noError = await handleValidationChecks(formFields)
+    let panVerified = undefined;
+    if (noError) {
+     panVerified = await verifyPan();  
+    } 
     setLoader(false)
     
-    if (noError) {
+    if (noError && panVerified) {
       Navigate('/depositetaker/signup/entitydetails')
     }
   };  
@@ -57,6 +87,7 @@ const VerificationForm = (props: Props) => {
                       case 'text':
                       case 'number':
                       case 'password':
+                      case "phone_number":
                         return <div>
                           <label
                             htmlFor={field?.label}
@@ -74,6 +105,7 @@ const VerificationForm = (props: Props) => {
                             type={fieldType}
                             id={field?.label}
                             placeholder={field?.placeholder}
+                            disabled={field?.disabled || false}
                           />
                           <span className="text-red-500">
                             {field?.error}
@@ -121,7 +153,34 @@ const VerificationForm = (props: Props) => {
                         <span className="text-red-500">
                             {field?.error}
                           </span>
-                      </div>                          
+                      </div>  
+                      
+                      case 'pincode':
+                        return <div>
+                          <label
+                            htmlFor={field?.label}
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                          >
+                            {field?.label}
+                            {field?.regFormFieldsValidations && 
+                              field?.regFormFieldsValidations?.some((v : any) => v?.id === allFormData?.validations?.find((d : any) => d?.vld_type_name === "Required")?.id)
+                              &&
+                              <span className="text-[#ff0000]">*</span>}
+                          </label>
+                          <InputFields
+                            max={6}
+                            min={6}
+                            value={field?.userInput}
+                            onChange={(e) => onChange(e, field, fieldType)}
+                            type={"number"}
+                            id={field?.label}
+                            placeholder={field?.placeholder}
+                          />
+                          <span className="text-red-500">
+                            {field?.error}
+                          </span>
+                        </div>
+
                       default:
                         return <></>;
                     }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,3 +2,4 @@ export const authBaseUrl = "http://34.160.37.43/api/v1";
 export const backendBaseUrl = "http://34.149.91.231";
 export const bffUrl = "http://34.36.40.227/api/v1";
 export const backendBudsPortalBFFUrl = "http://34.49.240.236/buds-portal"
+export const pincodeValidationUrl = "https://api.postalpincode.in/pincode/"
