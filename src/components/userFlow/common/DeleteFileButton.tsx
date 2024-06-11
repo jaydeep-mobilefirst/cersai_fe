ed1bd9870800e5f47d731ac1c30081bd9d0f2815
owaisahmedmalla <owais.mobilefirst@gmail.com>
2024-06-11 19:04:34 +0530

    bugs-fixing

--- src/components/userFlow/common/DeleteFileButton.tsx
+++ src/components/userFlow/common/DeleteFileButton.tsx
@@ -4,60 +4,58 @@ import LoaderSpin from "../../LoaderSpin";
 import Swal from "sweetalert2";
 
 type Props = {
-  fieldData: any,
-  fieldType: string
+  fieldData: any;
+  fieldType: string;
   onFileChange?: (
     event: any | undefined,
     field: any,
     fieldType: string
   ) => Promise<void>;
-}
+};
 
 const DeleteFileButton = ({ fieldData, fieldType, onFileChange }: Props) => {
   const [loader, setLoader] = useState<boolean>(false);
   const deleteFile = async () => {
     Swal.fire({
-      icon : "warning",
-      title : "Are you sure",
-      showCancelButton : true,
-      reverseButtons : true,
-      confirmButtonText : "Delete",
-      confirmButtonColor : "blue",
+      icon: "warning",
+      title: "Are you sure you want to delete",
+      showCancelButton: true,
+      reverseButtons: true,
+      confirmButtonText: "Delete",
+      confirmButtonColor: "blue",
     })
-    .then(async (res) => {
-      if (!res?.isConfirmed) {
-        return;
-      }
-      if (onFileChange) {
-      setLoader(true)
-      
-      await onFileChange("", fieldData, fieldType)
-      setLoader(false)
-    }
-      
-    })
-    .catch(res => {
-      console.log({res});
-      
-    })
-   
-  }
-  return (
-    <div onClick={deleteFile}
-    className="bg-white mt-1 mr-1 flex justify-center items-center h-10 w-10 rounded">
-      {
-        loader === true ?
-          <LoaderSpin />
-          :
-          <img
-            src={trashIcon}
-            onError={() => trashIcon}
-            alt="Delete"
-            className="rounded h-5 cursor-pointer"
-          />
-      }
-    </div>
-  )
-}
+      .then(async (res) => {
+        if (!res?.isConfirmed) {
+          return;
+        }
+        if (onFileChange) {
+          setLoader(true);
 
-export default DeleteFileButton
+          await onFileChange("", fieldData, fieldType);
+          setLoader(false);
+        }
+      })
+      .catch((res) => {
+        console.log({ res });
+      });
+  };
+  return (
+    <div
+      onClick={deleteFile}
+      className="bg-white mt-1 mr-1 flex justify-center items-center h-10 w-10 rounded"
+    >
+      {loader === true ? (
+        <LoaderSpin />
+      ) : (
+        <img
+          src={trashIcon}
+          onError={() => trashIcon}
+          alt="Delete"
+          className="rounded h-5 cursor-pointer"
+        />
+      )}
+    </div>
+  );
+};
+
+export default DeleteFileButton;
