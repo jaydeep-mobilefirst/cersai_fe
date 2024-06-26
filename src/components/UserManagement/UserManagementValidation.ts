f32677e1f985f7f3622ecb341df7a2167a26e0f3
DESKTOP-GFC1RS9\dell <rohan.mobilefirst@gmail.com>
2024-06-26 19:16:38 +0530

    UAM Edit user done

--- src/components/UserManagement/UserManagementValidation.ts
+++ src/components/UserManagement/UserManagementValidation.ts
@@ -4,9 +4,7 @@ export const UserManagementValidation = Yup.object().shape({
   firstName: Yup.string()
     .required("First name is required")
     .min(2, "First name must be at least 2 characters long"),
-  middleName: Yup.string()
-    .required("Middle name is required")
-    .min(2, "Middle name must be at least 2 characters long"),
+  middleName: Yup.string().trim(),
   lastName: Yup.string()
     .required("Last name is required")
     .min(2, "Last name must be at least 2 characters long"),
