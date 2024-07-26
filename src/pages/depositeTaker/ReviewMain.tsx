e2e269e61fa3f00c17ce4615468f46aa9a4216fa
jaydeep-mobilefirst <jaydeep.mobilefirst@gmail.com>
2024-07-26 16:16:17 +0530

    Update ReviewMain.tsx

--- src/pages/depositeTaker/ReviewMain.tsx
+++ src/pages/depositeTaker/ReviewMain.tsx
@@ -107,9 +107,9 @@ const ReviewMain = () => {
         const data = response.data;
         if (data?.success) {
           // setSubmitModal( true)
-          setPara1(`Your registration request has been sent successfully and
-          approval/rejection of your registration will be informed to you
-          via email.`);
+          setPara1(
+            `Thank you for signing up. We have sent account activation link on registered email ID. Please continue setting up account.`
+          );
           setPara2(
             `Your registration acknowledgement ID is ${data?.data?.newDepositTaker?.uniqueId}`
           );
@@ -138,98 +138,12 @@ const ReviewMain = () => {
 
   return (
     <>
-      <div className="flex flex-col justify-between h-screen">
-        <header className="lg:p-[38px] border-b border-gray-200"></header>
-        <main className="flex-grow p-8 overflow-auto custom-scrollbar">
-          <div id="reviewContent">
-            <h1 className="text-2xl font-bold mb-6 font-bold">Review</h1>
-            {/* {allFormData &&
-              allFormData?.entitySections?.map(
-                (section: any, index: number) => (
-                  allFormData?.formFields?.form_fields
-                    ?.filter((f: any) => f?.sectionId === section?.id)?.length > 0 &&
-                  <div className="mb-[16px]" key={index}>
-                    <div className="rounded-t-lg bg-[#E7F0FF] flex justify-between h-[57px]">
-                      <p className="lg:w-[152px] ml-[16px] mt-[16px] text-[20px] lg:text-[20px] pb-2 text-nowrap font-bold text-2xl" >
-                        {section?.sectionName}
-                      </p>
-                      <button className="text-[#1C468E] text-[16px] lg:text-[20px] mr-[13px] font-bold">
-                        {section?.sectionName !== "Verification" ? (
-                          <Link
-                            to={
-                              signupSideBar.find(
-                                (sec) =>
-                                  sec?.description === section?.sectionName
-                              )?.path + "?edit=true"
-                            }
-                          >
-                            Edit
-                          </Link>
-                        ) : (
-                          "Success"
-                        )}
-                      </button>
-                    </div>
+      <div className='flex flex-col justify-between h-screen'>
+        <header className='lg:p-[38px] border-b border-gray-200'></header>
+        <main className='flex-grow p-8 overflow-auto custom-scrollbar'>
+          <div id='reviewContent'>
+            <h1 className='text-2xl font-bold mb-6 font-bold'>Review</h1>
 
-                    <div className="shadow-sm p-5 rounded-md ">
-                      <div className="flex flex-col justify-between w-full sm:flex-row gap-y-[16px] text-gilroy-medium">
-                        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                          {allFormData?.formFields?.form_fields
-                            ?.filter((f: any) => f?.sectionId === section?.id)
-                            ?.map((field: any, idx: number) => {
-                              console.log({
-                                field,
-                              });
-
-                              return (
-                                <div
-                                  className={`sm:mr-[48px] flex justify-between ${idx % 2 === 0
-                                      ? "sm:border-r-[0.5px] border-r-[#385723] border-opacity-20"
-                                      : ""
-                                    } `}
-                                  key={idx}
-                                >
-                                  <div className="text-gray-500">
-                                    {field.label}
-                                    <span className="text-[#ff0000]">*</span>
-                                  </div>
-                                  <div>
-                                    {field?.dscFileNAme !== "" &&
-                                      field?.dscFileNAme !== undefined
-                                      ? field?.dscFileNAme
-                                      : field.userInput}
-                                  </div>
-                                </div>
-                              );
-                            })}
-                          {
-                            section?.sectionName === "Upload Documents" &&
-                            documentData?.map((doc: any, idx: number) => {
-                              return <div
-                                className={`sm:mr-[48px] flex justify-between ${idx % 2 === 0
-                                    ? "sm:border-r-[0.5px] border-r-[#385723] border-opacity-20"
-                                    : ""
-                                  } `}
-                                key={idx}
-                              >
-                                <div className="text-gray-500">
-                                  {doc?.documentName}
-                                  <span className="text-[#ff0000]">*</span>
-                                </div>
-                                <div>
-                                  {
-                                    doc?.fileName
-                                  }
-                                </div>
-                              </div>
-                            })
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )
-              )} */}
             <ReviewMainListing
               allFormData={allFormData}
               documentData={documentData}
@@ -237,17 +151,17 @@ const ReviewMain = () => {
               isPdfMode={isPdfMode}
             />
             {!isPdfMode && (
-              <div className="flex flex-shrink-0 mt-[20px] justify-start items-center">
-                <div className="">
+              <div className='flex flex-shrink-0 mt-[20px] justify-start items-center'>
+                <div className=''>
                   <input
-                    type="checkbox"
-                    className="h-4 w-4 accent-[#1c648e]"
+                    type='checkbox'
+                    className='h-4 w-4 accent-[#1c648e]'
                     checked={isChecked}
                     onChange={handleCheckboxChange}
-                    placeholder="ischecked"
+                    placeholder='ischecked'
                   />
                 </div>
-                <div className="leading-[24px] ml-4 text-gilroy-medium text-[14px]">
+                <div className='leading-[24px] ml-4 text-gilroy-medium text-[14px]'>
                   I here by declare that all information provided is best of my
                   knowledge
                 </div>
@@ -256,25 +170,25 @@ const ReviewMain = () => {
           </div>
         </main>
 
-        <div className="flex justify-between items-center my-3 flex-col sm:flex-row">
-          <div className=" ml-5">
+        <div className='flex justify-between items-center my-3 flex-col sm:flex-row'>
+          <div className=' ml-5'>
             <button
-              className="text-gilroy-regular text-sm flex items-center p-4 sm:p-0"
-              role="button"
+              className='text-gilroy-regular text-sm flex items-center p-4 sm:p-0'
+              role='button'
               onClick={() => Navigate("/depositetaker/signup/nodaldetails")}
             >
-              <img src={Arrow} alt="back Arrow" className="mr-2" />
+              <img src={Arrow} alt='back Arrow' className='mr-2' />
               Back
             </button>
           </div>
-          <div className="flex mr-9">
+          <div className='flex mr-9'>
             <div>
               <button
                 onClick={downloadPDF}
                 disabled={!isChecked}
-                className="w-auto md:w-[208px] md:h-[48px] gap-[8px] text-gilroy-semibold flex rounded-[12px] text-[#1C468E] border border-[#1C468E] p-3 md:pt-[12px] md:pr-[22px] md:pb-[12px] md:pl-[22px]"
+                className='w-auto md:w-[208px] md:h-[48px] gap-[8px] text-gilroy-semibold flex rounded-[12px] text-[#1C468E] border border-[#1C468E] p-3 md:pt-[12px] md:pr-[22px] md:pb-[12px] md:pl-[22px]'
               >
-                <img src={download} alt="download" className="mr-2" />
+                <img src={download} alt='download' className='mr-2' />
                 {isDownloading ? "Downloading..." : "Download PDF"}
               </button>
             </div>
@@ -304,8 +218,8 @@ const ReviewMain = () => {
           para2={para2}
           success={submitted}
         />
-        <footer className="p-4 border-[#E6E6E6] border-[1px] ">
-          <p className="text-gilroy-light text-center text-[#24222B] text-xs cursor-pointer mt-4">
+        <footer className='p-4 border-[#E6E6E6] border-[1px] '>
+          <p className='text-gilroy-light text-center text-[#24222B] text-xs cursor-pointer mt-4'>
             © 2024 Protean BUDs, All Rights Reserved.
           </p>
         </footer>
