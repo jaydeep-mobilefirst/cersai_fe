ab5e7d1f95ae0379e4fd4de81e8f36928d3b71d1
jaydeep-mobilefirst <jaydeep.mobilefirst@gmail.com>
2024-09-27 18:32:55 +0530

    Update SelectButton.tsx

--- src/components/ScehmaManagement/SelectButton.tsx
+++ src/components/ScehmaManagement/SelectButton.tsx
@@ -175,7 +175,7 @@ const SelectButtonTask = ({
     <div className={`relative w-full ${mdWidth}`} ref={dropdownRef}>
       <button
         className={`w-full flex justify-between items-center border rounded-md shadow-sm px-4 py-2 text-gray-700 hover:bg-gray-50 focus:ring-1 focus:ring-gray-300 text-left ${mdWidth}`}
-        type="button"
+        type='button'
         onClick={handleButtonClick}
         style={{
           height: "50px",
@@ -186,36 +186,41 @@ const SelectButtonTask = ({
           backgroundColor: bgColor,
         }}
       >
-        {selectedOption ? selectedOption : placeholder}
+        {selectedOption === "UNDER_LETIGATION"
+          ? "Under litigation"
+          : selectedOption
+          ? selectedOption
+          : placeholder}
+        {}
         <div>
           {!arrowDirectionToggle ? (
             <svg
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+              className='w-4 h-4 ml-2 -mr-1'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 strokeWidth={2}
-                d="M19 9l-7 7-7-7"
+                d='M19 9l-7 7-7-7'
               />
             </svg>
           ) : (
             <svg
-              className="w-4 h-4 ml-2 -mr-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+              className='w-4 h-4 ml-2 -mr-1'
+              fill='none'
+              stroke='currentColor'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 strokeWidth={2}
-                d="M5 15l7-7 7 7"
+                d='M5 15l7-7 7 7'
               ></path>
             </svg>
           )}
@@ -224,26 +229,26 @@ const SelectButtonTask = ({
       {arrowDirectionToggle && (
         <div
           className={`absolute w-full ${mdWidth} rounded-md bg-white shadow-lg`}
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="options-menu"
+          role='menu'
+          aria-orientation='vertical'
+          aria-labelledby='options-menu'
           style={{
             zIndex: 999,
             border: `1px solid ${borderColor}`,
           }}
         >
           <div
-            className="overflow-auto max-h-40 custom-scrollbar"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
+            className='overflow-auto max-h-40 custom-scrollbar'
+            role='menu'
+            aria-orientation='vertical'
+            aria-labelledby='options-menu'
           >
             {options.map((option, index) => (
               <a
                 key={index}
                 onClick={() => handleOptionClick(option)}
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-green-50 hover:text-gray-900 cursor-pointer"
-                role="menuitem"
+                className='block px-4 py-2 text-sm text-gray-700 hover:bg-green-50 hover:text-gray-900 cursor-pointer'
+                role='menuitem'
               >
                 {option.label}
               </a>
