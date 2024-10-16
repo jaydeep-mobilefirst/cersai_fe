4cb41aa961282e76c42c580869d5fb77a45de219
jaydeep-mobilefirst <jaydeep.mobilefirst@gmail.com>
2024-10-17 01:49:29 +0530

    Update FooterDT.tsx

--- src/pages/mainPortal/Edit Profile/FooterDT.tsx
+++ src/pages/mainPortal/Edit Profile/FooterDT.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import LoaderSpin from "../../../components/LoaderSpin";
 import useStore from "../../../store/statusStore";
@@ -60,67 +60,70 @@ const FooterDT: React.FC<FooterProps> = ({
         {showbackbtn && (
           <>
             <div
-              className="flex flex-row items-center space-x-2"
+              className='flex flex-row items-center space-x-2'
               // onClick={() => Navigate(path)}
               onClick={backNavigation}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                className="shrink-0"
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                className='shrink-0'
               >
                 <path
-                  d="M15 6L9 12L15 18"
-                  stroke="#1D1D1B"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  d='M15 6L9 12L15 18'
+                  stroke='#1D1D1B'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                 />
               </svg>
-              <button className="text-black transition duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#385723] text-gilroy-regular">
+              <button className='text-black transition duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#385723] text-gilroy-regular'>
                 Back
               </button>
             </div>
           </>
         )}
 
-        <div className="flex items-center">
+        <div className='flex items-center'>
           <>
-          {data?.profileUpdate == "false" && (<><button
-                disabled={disabled}
-                onClick={onSubmit}
-                type="submit"
-                className={`${
-                  disabled ? "bg-gray-500" : "bg-[#1C468E] mx-3"
-                } rounded-xl p-3 text-white font-semibold text-sm w-full sm:w-auto sm:max-w-xs`}
-              >
-                {loader ? <LoaderSpin /> : "Save and continue"}
-              </button></>)}
-          
+            {data?.profileUpdate == true && (
+              <>
+                <button
+                  disabled={disabled}
+                  onClick={onSubmit}
+                  type='submit'
+                  className={`${
+                    disabled ? "bg-gray-500" : "bg-[#1C468E] mx-3"
+                  } rounded-xl p-3 text-white font-semibold text-sm w-full sm:w-auto sm:max-w-xs`}
+                >
+                  {loader ? <LoaderSpin /> : "Save and continue"}
+                </button>
+              </>
+            )}
           </>
         </div>
       </div>
       <div>
-        <div className="border-[#E6E6E6] border-[1px] w-full"></div>
-        <div className="text-gilroy-light text-[#24222B] text-xs cursor-pointer h-16 flex items justify-center items-center">
-        <div className="text-center mt-auto">
-        <h1 className="text-[#24222B] text-xs text-wrap text-gilroy-light mt-3 font-normal">
-          COPYRIGHT © 2024 CERSAI. ALL RIGHTS RESERVED.
-        </h1>
-        <p className="text-[#24222B] text-xs text-wrap text-gilroy-light font-normal">
-          Powered and managed by{" "}
-          <a
-            href="https://www.proteantech.in/"
-            className="underline text-gilroy-regular font-bold"
-            target="_blank"
-          >
-            Protean eGov Technologies
-          </a>{" "}
-        </p>
-      </div>
+        <div className='border-[#E6E6E6] border-[1px] w-full'></div>
+        <div className='text-gilroy-light text-[#24222B] text-xs cursor-pointer h-16 flex items justify-center items-center'>
+          <div className='text-center mt-auto'>
+            <h1 className='text-[#24222B] text-xs text-wrap text-gilroy-light mt-3 font-normal'>
+              COPYRIGHT © 2024 CERSAI. ALL RIGHTS RESERVED.
+            </h1>
+            <p className='text-[#24222B] text-xs text-wrap text-gilroy-light font-normal'>
+              Powered and managed by{" "}
+              <a
+                href='https://www.proteantech.in/'
+                className='underline text-gilroy-regular font-bold'
+                target='_blank'
+              >
+                Protean eGov Technologies
+              </a>{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
